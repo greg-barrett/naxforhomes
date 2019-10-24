eb7b76b77c999b20258d7eef00de0a5e7abdd86f
Gregory Barrett <gbarrett15@gmail.com>
2019-10-24 18:19:04 +0200

    try moving images

--- app/controllers/downloads_controller.rb
+++ app/controllers/downloads_controller.rb
@@ -1,19 +1,19 @@
 class DownloadsController < ApplicationController
   def isoone
-    send_file "#{Rails.root}/public/downloads/Certificado_ISO_9001_2015_to_2021.pdf", :type => 'application/pdf', :disposition => 'attachment'
+    send_file "#{Rails.root}/app/assets/downloads/Certificado_ISO_9001_2015_to_2021.pdf", :type => 'application/pdf', :disposition => 'attachment'
   end
 
   def isotwo
-    send_file "#{Rails.root}/public/downloads/Certificado_ISO_14001_2015_to_2020.pdf", :type => 'pdf', :disposition => 'attachment'
+    send_file "#{Rails.root}/app/assets/downloads/Certificado_ISO_14001_2015_to_2020.pdf", :type => 'pdf', :disposition => 'attachment'
 
   end
 
   def isothree
-    send_file "#{Rails.root}/public/downloads/Certificado_ISO_45001_2018_to_2021.pdf", :type => 'pdf', :disposition => 'attachment'
+    send_file "#{Rails.root}/app/assets/downloads/Certificado_ISO_45001_2018_to_2021.pdf", :type => 'pdf', :disposition => 'attachment'
 
   end
 
   def quality
-    send_file "#{Rails.root}/public/downloads/Politica_Integrada.pdf", :type => 'pdf', :disposition => 'attachment'
+    send_file "#{Rails.root}/app/assets/downloads/Politica_Integrada.pdf", :type => 'pdf', :disposition => 'attachment'
   end
 end
