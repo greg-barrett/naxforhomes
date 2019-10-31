1327bc2ae24117d0087e6f6f2e0bdb13f6f1ce4e
Gregory Barrett <gbarrett15@gmail.com>
2019-10-31 14:06:37 +0100

    config vars

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -52,3 +52,12 @@ addEventListener("direct-upload:end", event => {
   const element = document.getElementById(`direct-upload-${id}`)
   element.classList.add("direct-upload--complete")
 })
+
+function initMap(lat, lng) {
+    var myCoords = new google.maps.LatLng(lat, lng);
+    var mapOptions = {
+    center: myCoords,
+    zoom: 14
+    };
+    var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+}
