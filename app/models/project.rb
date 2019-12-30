1271bb28d96c9f44eca1de805f68258550b2275c
Gregory Barrett <gbarrett15@gmail.com>
2019-12-30 12:13:22 +0100

    updates google maps resize location order results

--- app/models/project.rb
+++ app/models/project.rb
@@ -4,6 +4,7 @@ class Project < ApplicationRecord
   translates :description, :ptype, :fallbacks_for_empty_translations => true
   accepts_nested_attributes_for :features, :allow_destroy => true, :reject_if => :all_blank
   validates :name, :location, :description, :presence => true
+  default_scope { order(created_at: :desc) }
 
   def resized_images(input)
     return self.images.find(input.id).variant(resize: '1920x1080', quality: '90').processed
